c42214b1e084101e993fb04450189551ce8db692
Tomas Maldonis <tomasis7@gmail.com>
2025-05-14 13:44:33 +0200

    Implement CartContext for managing cart state and functionality

---
+++ context/CartContext.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import { CartItem, Product } from "@/data";
+import { Alert, Snackbar, Typography } from "@mui/material";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+interface ContextValues {
+  cartItems: CartItem[];
+  cartCount: number;
+  totalSum: number;
+  updateQuantity: (id: string, amount: number) => void;
+  addToCart: (item: Product) => void;
+  removeFromCart: (itemId: string) => void;
+  clearCart: () => void;
+  showToast: (message: string) => void;
+}
+
+const CartContext = createContext({} as ContextValues);
+
+export function CartProvider(props: PropsWithChildren) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [toastMessage, setToastMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    const cart = localStorage.getItem("cart");
+    if (cart) {
+      setCartItems(JSON.parse(cart));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  const showToast = (message: string) => {
+    setToastMessage(message);
+  };
+
+  const addToCart = (item: Product) => {
+    setCartItems((prevItems) => {
+      const existingItemIndex = prevItems.findIndex(
+        (cartItem) => cartItem.id === item.id
+      );
+      if (existingItemIndex !== -1) {
+        const updatedItems = [...prevItems];
+
+        updatedItems[existingItemIndex] = {
+          ...updatedItems[existingItemIndex],
+          quantity: updatedItems[existingItemIndex].quantity + 1,
+        };
+        return updatedItems;
+      } else {
+        const newItem = {
+          ...item,
+          quantity: 1,
+        };
+        return [...prevItems, newItem];
+      }
+    });
+
+    showToast("Produkten har lagts till i kundvagnen!");
+  };
+
+  const removeFromCart = (id: string) => {
+    setCartItems((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
+
+  const updateQuantity = (id: string, amount: number) => {
+    setCartItems((prevCart) => {
+      return prevCart
+        .map((item) =>
+          item.id === id ? { ...item, quantity: item.quantity + amount } : item
+        )
+        .filter((item) => item.quantity !== 0);
+    });
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+    localStorage.removeItem("cart");
+  };
+
+  const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0);
+
+  useEffect(() => {
+    console.log("cartCount updated:", cartCount);
+  }, [cartCount]);
+
+  const totalSum = cartItems.reduce(
+    (sum, item) => sum + item.quantity * item.price,
+    0
+  );
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        cartCount,
+        totalSum,
+        updateQuantity,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        showToast,
+      }}
+    >
+      {props.children}
+      {toastMessage && (
+        <Snackbar
+          open
+          autoHideDuration={2000}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          sx={{
+            width: "auto",
+            maxWidth: "350px",
+            borderRadius: "0.5rem",
+            boxShadow: "none",
+          }}
+        >
+          <Alert
+            onClose={() => setToastMessage(null)}
+            severity="success"
+            variant="outlined"
+            sx={{
+              width: "100%",
+              borderRadius: "0.5rem",
+              borderColor: "success.main",
+              color: "success.main",
+              backgroundColor: "white",
+              fontSize: "1rem",
+              fontWeight: "bold",
+              padding: "6px 16px",
+            }}
+            data-cy="added-to-cart-toast"
+          >
+            <Typography variant="body1">{toastMessage}</Typography>
+          </Alert>
+        </Snackbar>
+      )}
+    </CartContext.Provider>
+  );
+}
+
+export default CartContext;
