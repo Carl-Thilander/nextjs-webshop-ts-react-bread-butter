3e1bd57645529f5775ad58b6e205f6d829c8bff2
Anna <anna.kvernplassen@gmail.com>
2025-05-18 21:06:02 +0200

    add date to order in prisma, map orders into table and mount the table in the orders page

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,21 @@
+import { Container } from "@mui/material";
+
+export default async function AllOrders() {
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        bgcolor: "background.paper",
+        marginTop: 2,
+        marginBottom: 2,
+        borderRadius: 2,
+        padding: 4,
+        border: "2px solid #9C8173",
+      }}
+    >
+      <AdminOrderTable />
+    </Container>
+  );
+}
