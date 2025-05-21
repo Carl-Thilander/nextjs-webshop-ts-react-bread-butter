ef69092f1da06cd69d56a5cbfe884d5b9826d5b9
VickyPhu <victoriaphu@gmail.com>
2025-05-21 13:41:06 +0200

    changed to IconButton, removed old css

--- app/components/add-to-cart-button.tsx
+++ app/components/add-to-cart-button.tsx
@@ -1,41 +1,40 @@
 "use client";
 
-import { Button } from "@mui/material";
-import { Product } from "@prisma/client";
 import { useCart } from "@/hooks/useCart";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import { IconButton } from "@mui/material";
+import { Product } from "@prisma/client";
 
 interface AddToCartButtonProps {
-  label?: string;
-  product: Product;
+	label?: string;
+	product: Product;
 }
 
 const AddToCartButton: React.FC<AddToCartButtonProps> = ({
-  label = "Lägg i kundvagn",
-  product,
+	label = "Lägg i kundvagn",
+	product,
 }) => {
-  const { addToCart, showToast } = useCart();
+	const { addToCart, showToast } = useCart();
 
-  const handleAddToCart = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.stopPropagation();
-    event.preventDefault();
-    addToCart(product);
-    showToast("Produkten har lagts till i kundvagnen!");
-  };
+	const handleAddToCart = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.stopPropagation();
+		event.preventDefault();
+		addToCart(product);
+		showToast("Produkten har lagts till i kundvagnen!");
+	};
 
-  return (
-    <Button
-      onClick={handleAddToCart}
-      size="large"
-      sx={{
-        mt: 1,
-        bgcolor: "primary.main",
-        color: "text.primary",
-        "&:hover": { bgcolor: "primary.dark", color: "background.paper" },
-      }}
-    >
-      {label}
-    </Button>
-  );
+	return (
+		<IconButton
+			onClick={handleAddToCart}
+			sx={{
+        p: 0,
+				color: "text.primary",
+				"&:hover": { transform: "scale(1.25)" },
+			}}
+		>
+			<LocalMallIcon sx={{fontSize: "1.8rem"}}/>
+		</IconButton>
+	);
 };
 
 export default AddToCartButton;
