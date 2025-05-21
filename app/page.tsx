357c2bc193866800ee2552d19e9d436fe1c1fab8
VickyPhu <victoriaphu@gmail.com>
2025-05-21 14:33:11 +0200

    adjusted grid gaps

--- app/page.tsx
+++ app/page.tsx
@@ -22,12 +22,11 @@ export default async function Home() {
 
       <CategorySection />
       <Container
+      disableGutters
         sx={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-
-          minHeight: "100vh",
         }}
       >
         <Box
@@ -37,16 +36,15 @@ export default async function Home() {
             flexGrow: 1,
             borderRadius: "0.5rem",
             padding: 4, //Mått vi förmodligen vill använda i hela appen. (1=8px)
-            margin: "2rem 0",
-            width: "100%",
+
           }}
         >
           <Grid
             container
             direction="row"
             sx={{ justifyContent: "center", alignItems: "center" }}
-            spacing={{ xs: 2, md: 3 }}
-            columns={{ xs: 12, sm: 6, md: 4 }}
+            rowSpacing={{ xs: 3, sm: 4, md: 5 }}
+            columnSpacing={{ xs: 12, sm: 4, md: 4 }}
           >
             {products.map((product) => (
               <Link
