d8924257c1f22ea4f249eb14b0da8b8d1625e2f6
Tomas Maldonis <tomasis7@gmail.com>
2025-05-22 16:07:18 +0200

    refactor: migrate database operations from db to prisma across multiple files

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,10 @@
-import { db } from "@/prisma/db";
+import { prisma } from "@/prisma/db";
 import AdminItem from "./admin-item-card";
 import AddProductButton from "./buttons/add-product-button";
 import { Button, Typography } from "@mui/material";
 
 export default async function AdminPage() {
-  const products = await db.product.findMany();
+  const products = await prisma.product.findMany();
 
   return (
     <>
