8049f628f83ce576c37f757d392e1bd4720de457
VickyPhu <victoriaphu@gmail.com>
2025-05-22 16:58:28 +0200

    added out of stock message in modal

--- app/checkout/out-of-stock-modal.tsx
+++ app/checkout/out-of-stock-modal.tsx
@@ -10,15 +10,27 @@ import {
 interface Props {
 	open: boolean;
 	onClose: () => void;
+	productName?: string;
+	available?: number;
+	requested?: number;
 }
 
-export default function OutOfStockModal({ open, onClose }: Props) {
+export default function OutOfStockModal({
+	open,
+	onClose,
+	productName,
+	available,
+	requested,
+}: Props) {
 	return (
 		<Dialog open={open} onClose={onClose}>
 			<DialogTitle>Product not available</DialogTitle>
 			<DialogContent>
-				{/* change this */}
-				<DialogContentText>Product is out of stock</DialogContentText>
+				<DialogContentText>
+					{productName
+						? `You have ${requested} pcs of ${productName} in your cart, but only ${available} pcs is in stock. Please update your cart`
+						: `One or more products in your cart are out of stock`}
+				</DialogContentText>
 			</DialogContent>
 			<DialogActions>
 				<Button onClick={onClose} autoFocus>
