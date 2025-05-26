c23d1e558cbbf273c5217467c2396a98dcaaf5da
VickyPhu <victoriaphu@gmail.com>
2025-05-26 10:04:07 +0200

    css on dialog title

--- app/checkout/out-of-stock-modal.tsx
+++ app/checkout/out-of-stock-modal.tsx
@@ -6,7 +6,6 @@ import {
 	DialogContentText,
 	DialogTitle,
 } from "@mui/material";
-import { useState } from "react";
 
 export interface OutOfStockItems {
 	productName: string;
@@ -23,15 +22,15 @@ interface Props {
 export default function OutOfStockModal({ open, onClose, items = [] }: Props) {
 	return (
 		<Dialog open={open} onClose={onClose}>
-			<DialogTitle>Products not available</DialogTitle>
-			<DialogContent dividers>
+			<DialogTitle variant="h5" textAlign="center" sx={{fontWeight: "bold"}}>Products not available</DialogTitle>
+			<DialogContent>
 				{items.length === 0 ? (
-					<DialogContentText>
+					<DialogContentText textAlign="center">
 						One or more products in your cart are out of stock.
 					</DialogContentText>
 				) : (
 					items.map(({ productName, available, requested }) => (
-						<DialogContentText key={productName}>
+						<DialogContentText textAlign="center" key={productName}>
 							You have {requested} psc of {productName} in your cart, but only{" "}
 							{available} psc is in stock. Please update your cart.
 						</DialogContentText>
