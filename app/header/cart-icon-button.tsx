0dd0655d80c607dabe90bfd696fbe49adf283ebc
VickyPhu <victoriaphu@gmail.com>
2025-05-30 14:25:28 +0200

    added useEffect to close the drawer when navigating to another page

--- app/header/cart-icon-button.tsx
+++ app/header/cart-icon-button.tsx
@@ -1,113 +1,121 @@
+import { useCart } from "@/hooks/useCart";
 import { Close } from "@mui/icons-material";
 import {
-  Backdrop,
-  Box,
-  Divider,
-  Drawer,
-  IconButton,
-  Typography,
+	Backdrop,
+	Box,
+	Divider,
+	Drawer,
+	IconButton,
+	Typography,
 } from "@mui/material";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import CartList from "../cart/cart-list";
 import CartSummary from "../cart/cart-summary";
 import CartBadge from "../components/cart-badge";
-import { useCart } from "@/hooks/useCart";
 
 export default function MyDrawer() {
-  const [open, setOpen] = useState(false);
-  const { cartCount } = useCart();
+	const [open, setOpen] = useState(false);
+	const { cartCount } = useCart();
+	const pathname = usePathname();
 
-  const toggleDrawer = (open: boolean) => () => {
-    setOpen(open);
-  };
+	useEffect(() => {
+		const pathsToClose = ["/checkout", "/auth/register"];
+		if (pathsToClose.includes(pathname)) {
+			setOpen(false);
+		}
+	}, [pathname]);
 
-  const router = useRouter();
+	const toggleDrawer = (open: boolean) => () => {
+		setOpen(open);
+	};
 
-  return (
-    <>
-      <IconButton
-        onClick={toggleDrawer(true)}
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <CartBadge />
-      </IconButton>
+	const router = useRouter();
 
-      {/* Backdrop for darkened background */}
-      <Backdrop
-        open={open}
-        sx={{
-          zIndex: 1300, // Should be behind the drawer but in front of the rest of the page
-          color: "#fff",
-          backgroundColor: "rgba(0, 0, 0, 0.5)", // Dark background for everything behind the drawer
-        }}
-        onClick={toggleDrawer(false)} // Close the drawer when backdrop is clicked
-      />
+	return (
+		<>
+			<IconButton
+				onClick={toggleDrawer(true)}
+				sx={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<CartBadge />
+			</IconButton>
 
-      {/* Drawer */}
-      <Drawer
-        anchor="right"
-        open={open}
-        onClose={toggleDrawer(false)}
-        sx={{
-          zIndex: 1301,
-          width: { xs: "50%", sm: 400, md: 500 },
-          maxWidth: 320,
-          maxHeight: "80vh",
-          overflowY: "auto",
-        }}
-      >
-        <Box
-          sx={{
-            width: "100%",
-            paddingY: 3,
-            backgroundColor: "background.paper",
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-          }}
-        >
-          {/* Close button */}
-          <IconButton
-            onClick={toggleDrawer(false)}
-            sx={{
-              position: "absolute",
-              top: 5,
-              right: 7,
-              color: "text.primary",
-            }}
-          >
-            <Close />
-          </IconButton>
+			{/* Backdrop for darkened background */}
+			<Backdrop
+				open={open}
+				sx={{
+					zIndex: 1300, // Should be behind the drawer but in front of the rest of the page
+					color: "#fff",
+					backgroundColor: "rgba(0, 0, 0, 0.5)", // Dark background for everything behind the drawer
+				}}
+				onClick={toggleDrawer(false)} // Close the drawer when backdrop is clicked
+			/>
 
-          <Typography
-            variant="h5"
-            sx={{
-              fontFamily: "var(--font-montserrat)",
-              paddingLeft: 2,
-            }}
-          >
-            Cart
-          </Typography>
+			{/* Drawer */}
+			<Drawer
+				anchor="right"
+				open={open}
+				onClose={toggleDrawer(false)}
+				sx={{
+					zIndex: 1301,
+					width: { xs: "50%", sm: 400, md: 500 },
+					maxWidth: 320,
+					maxHeight: "80vh",
+					overflowY: "auto",
+				}}
+			>
+				<Box
+					sx={{
+						width: "100%",
+						paddingY: 3,
+						backgroundColor: "background.paper",
+						display: "flex",
+						flexDirection: "column",
+						gap: 2,
+					}}
+				>
+					{/* Close button */}
+					<IconButton
+						onClick={toggleDrawer(false)}
+						sx={{
+							position: "absolute",
+							top: 5,
+							right: 7,
+							color: "text.primary",
+						}}
+					>
+						<Close />
+					</IconButton>
 
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 2,
-              paddingInline: 2,
-            }}
-          >
-            <CartList />
-            <Divider sx={{ marginY: 2 }} />
-            <CartSummary />
-          </Box>
-        </Box>
-      </Drawer>
-    </>
-  );
+					<Typography
+						variant="h5"
+						sx={{
+							fontFamily: "var(--font-montserrat)",
+							paddingLeft: 2,
+						}}
+					>
+						Cart
+					</Typography>
+
+					<Box
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							gap: 2,
+							paddingInline: 2,
+						}}
+					>
+						<CartList />
+						<Divider sx={{ marginY: 2 }} />
+						<CartSummary />
+					</Box>
+				</Box>
+			</Drawer>
+		</>
+	);
 }
