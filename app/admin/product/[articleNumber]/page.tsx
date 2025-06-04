3370bd166853d3e2a72e95e3179f0c6eea70cacf
Omar-Alme <omar.almehdar17@gmail.com>
2025-06-04 10:27:12 +0200

    Remove revalidation for the root path in EditProductPage

--- app/admin/product/[articleNumber]/page.tsx
+++ app/admin/product/[articleNumber]/page.tsx
@@ -1,7 +1,6 @@
 import { prisma } from "@/prisma/db";
 import { Dialog } from "@mui/material";
 import ProductForm from "../../../components/products/product-form";
-import { revalidatePath } from "next/cache";
 
 interface Props {
   params: Promise<{ articleNumber: string }>;
@@ -14,8 +13,6 @@ export default async function EditProductPage({ params }: Props) {
     where: { articleNumber },
     include: { categories: true },
   });
-
-  revalidatePath("/");
 
   if (!product) return <main>404</main>;
   return (
